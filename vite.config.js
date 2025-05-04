44fe69c39fbe35eb38a3e39828346e13a50c7965
nzonzonzonzo <nzeigowens@gmail.com>
2025-05-04 17:13:03 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/nzonzonzonzo/pool-league-analyzer/', // Replace with your actual GitHub repository name
+})
+
 // Get the hostname from the error message
 const REPLIT_HOST =
   "5c9ce84a-949c-462c-b582-1d949f2209a0-00-34tv1tyh493iv.worf.replit.dev";
