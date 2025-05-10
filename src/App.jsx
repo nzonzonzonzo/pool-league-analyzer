df601fc24c147a9c11d3b1b739f8274731ecc168
nzonzonzonzo <nzeigowens@gmail.com>
2025-05-09 21:46:24 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -57,13 +57,14 @@ applyTheme(getInitialTheme());
 
 // SVG Icon Components
 const LightbulbIcon = () => (
+  <span className="text-neutral-500 hover:text-neutral-400 transition-colors z-30 cursor-pointer">
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="18"
     height="18"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="#bdc1c6"
+    stroke="currentColor" 
     strokeWidth="1"
     strokeLinecap="round"
     strokeLinejoin="round"
@@ -72,16 +73,18 @@ const LightbulbIcon = () => (
     <path d="M10 22h4"></path>
     <path d="M15.09 14c.18-.98.65-1.74 1.41-2.5A4.65 4.65 0 0 0 18 8A6 6 0 0 0 6 8c0 1 .23 2.23 1.5 3.5A4.61 4.61 0 0 1 8.91 14"></path>
   </svg>
+</span>
 );
 
 const CloseIcon = () => (
+  <span className="text-neutral-500 hover:text-neutral-400 transition-colors z-30 cursor-pointer">
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="18"
     height="18"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="#bdc1c6"
+    stroke="currentColor"
     strokeWidth="1"
     strokeLinecap="round"
     strokeLinejoin="round"
@@ -89,6 +92,7 @@ const CloseIcon = () => (
     <line x1="18" y1="6" x2="6" y2="18"></line>
     <line x1="6" y1="6" x2="18" y2="18"></line>
   </svg>
+  </span>
 );
 
 // Floating info button
